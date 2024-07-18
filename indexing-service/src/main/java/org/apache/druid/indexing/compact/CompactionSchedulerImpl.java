80443dca18096b52e0b4801f09b77b4c37550109
Kashif Faraz <kashif.faraz@gmail.com>
2024-07-18 14:44:16 +0530

    Fix dependencies

--- indexing-service/src/main/java/org/apache/druid/indexing/compact/CompactionSchedulerImpl.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/compact/CompactionSchedulerImpl.java
@@ -28,16 +28,15 @@ import org.apache.druid.client.indexing.IndexingTotalWorkerCapacityInfo;
 import org.apache.druid.client.indexing.TaskPayloadResponse;
 import org.apache.druid.common.config.JacksonConfigManager;
 import org.apache.druid.error.DruidException;
-import org.apache.druid.error.ErrorResponse;
 import org.apache.druid.indexer.TaskLocation;
 import org.apache.druid.indexer.TaskStatus;
 import org.apache.druid.indexer.TaskStatusPlus;
 import org.apache.druid.indexing.common.task.Task;
 import org.apache.druid.indexing.overlord.TaskMaster;
+import org.apache.druid.indexing.overlord.TaskQueryTool;
 import org.apache.druid.indexing.overlord.TaskQueue;
 import org.apache.druid.indexing.overlord.TaskRunner;
 import org.apache.druid.indexing.overlord.TaskRunnerListener;
-import org.apache.druid.indexing.overlord.http.OverlordResource;
 import org.apache.druid.java.util.common.CloseableIterators;
 import org.apache.druid.java.util.common.concurrent.Execs;
 import org.apache.druid.java.util.common.concurrent.ScheduledExecutorFactory;
@@ -59,7 +58,6 @@ import org.apache.druid.timeline.SegmentTimeline;
 import org.joda.time.Interval;
 
 import javax.annotation.Nullable;
-import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -70,6 +68,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 /**
  * TODO: pending items
@@ -88,8 +87,8 @@ public class CompactionSchedulerImpl implements CompactionScheduler
 {
   private static final Logger log = new Logger(CompactionSchedulerImpl.class);
 
-  private volatile TaskMaster taskMaster;
-  private volatile OverlordResource overlordResource;
+  private final TaskMaster taskMaster;
+  private final TaskQueryTool taskQueryTool;
   private final JacksonConfigManager configManager;
   private final SegmentsMetadataManager segmentManager;
 
@@ -110,12 +109,16 @@ public class CompactionSchedulerImpl implements CompactionScheduler
 
   @Inject
   public CompactionSchedulerImpl(
+      TaskMaster taskMaster,
+      TaskQueryTool taskQueryTool,
       SegmentsMetadataManager segmentManager,
       JacksonConfigManager configManager,
       CoordinatorOverlordServiceConfig coordinatorOverlordServiceConfig,
       ScheduledExecutorFactory executorFactory
   )
   {
+    this.taskMaster = taskMaster;
+    this.taskQueryTool = taskQueryTool;
     this.configManager = configManager;
     this.segmentManager = segmentManager;
     this.executor = executorFactory.create(1, "CompactionScheduler-%s");
@@ -142,18 +145,6 @@ public class CompactionSchedulerImpl implements CompactionScheduler
 
       }
     };
-  }
-
-  /**
-   * Injects required dependencies after instantiation. This is needed to avoid
-   * circular guice dependencies since TaskMaster already depends on
-   * CompactionScheduler to register it for leadership lifecycle.
-   */
-  @Inject(optional = true)
-  public void injectDependencies(TaskMaster taskMaster, OverlordResource overlordResource)
-  {
-    this.taskMaster = taskMaster;
-    this.overlordResource = overlordResource;
   }
 
   @LifecycleStart
@@ -325,6 +316,10 @@ public class CompactionSchedulerImpl implements CompactionScheduler
     }
   }
 
+  /**
+   * Dummy Overlord client used by the {@link #duty} to fetch task related info.
+   * This client simply redirects all queries to the {@link TaskQueryTool}.
+   */
   private class LocalOverlordClient extends NoopOverlordClient
   {
     @Override
@@ -354,16 +349,20 @@ public class CompactionSchedulerImpl implements CompactionScheduler
     @Override
     public ListenableFuture<TaskPayloadResponse> taskPayload(String taskId)
     {
-      return futureOf(overlordResource.getTaskPayload(taskId));
+      return futureOf(
+          () -> new TaskPayloadResponse(taskId, taskQueryTool.getTask(taskId).transform().orNull())
+      );
     }
 
     @Override
     public ListenableFuture<CloseableIterator<TaskStatusPlus>> taskStatuses(
-        @Nullable String state, @Nullable String dataSource, @Nullable Integer maxCompletedTasks
+        @Nullable String state,
+        @Nullable String dataSource,
+        @Nullable Integer maxCompletedTasks
     )
     {
       final ListenableFuture<List<TaskStatusPlus>> tasksFuture
-          = futureOf(overlordResource.getAllActiveTasks());
+          = futureOf(taskQueryTool.get);
       return Futures.transform(
           tasksFuture,
           taskList -> CloseableIterators.withEmptyBaggage(taskList.iterator()),
@@ -374,27 +373,23 @@ public class CompactionSchedulerImpl implements CompactionScheduler
     @Override
     public ListenableFuture<Map<String, List<Interval>>> findLockedIntervals(List<LockFilterPolicy> lockFilterPolicies)
     {
-      return futureOf(overlordResource.getDatasourceLockedIntervalsV2(lockFilterPolicies));
+      return futureOf(() -> taskQueryTool.getLockedIntervals(lockFilterPolicies));
     }
 
     @Override
     public ListenableFuture<IndexingTotalWorkerCapacityInfo> getTotalWorkerCapacity()
     {
-      return futureOf(overlordResource.getTotalWorkerCapacity());
+      return futureOf(() -> taskQueryTool.getTotalWorkerCapacity());
     }
 
     @SuppressWarnings("unchecked")
-    private <T> ListenableFuture<T> futureOf(Response response)
+    private <T> ListenableFuture<T> futureOf(Supplier<T> supplier)
     {
-      final int responseCode = response.getStatus();
-      if (responseCode >= 200 && responseCode < 300) {
-        return Futures.immediateFuture((T) response.getEntity());
-      } else if (response.getEntity() instanceof ErrorResponse) {
-        ErrorResponse error = (ErrorResponse) response.getEntity();
-        return Futures.immediateFailedFuture(error.getUnderlyingException());
-      } else {
-        log.warn("Error while invoking overlord method: [%s]", response.getEntity());
-        return Futures.immediateFailedFuture(DruidException.defensive("Unknown error"));
+      try {
+        return Futures.immediateFuture(supplier.get());
+      }
+      catch (Exception e) {
+        return Futures.immediateFailedFuture(e);
       }
     }
   }
